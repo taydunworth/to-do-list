fb0b4c501c269e34ff6ffa0742009538d742e8b2
Taylor Cox <taylorashtoncox@gmail.com>
2017-07-28 14:23:44 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const mustacheExpress = require('mustache-express')
+const app = express()
+const todos = ['Make the bed', 'wash the car']
+
+app.use(express.static('public'))
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.get('/', function(req, res) {
+  res.render('index', { todos: todos })
+})
+
+app.post('/', function(req, res) {
+  todos.push(req.body.todo)
+  res.redirect('/')
+})
+
+app.listen(3000, function() {
+  console.log('Successfully started express application!')
+})
